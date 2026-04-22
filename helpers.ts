26f914762a18aa62cec3d60a1873e539ebaa1aa0
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:52:26 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:52:03.743Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-22T03:52:26.207Z
+Commit: Fix broken links in documentation
