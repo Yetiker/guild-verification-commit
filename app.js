fef4de4d735d3d0b68fcf0c8eb9313f56bca01ef
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:51:30 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:51:18.706Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-22T03:51:29.859Z
+Commit: Fix CSS styling bug on mobile devices
