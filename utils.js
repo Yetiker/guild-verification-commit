fdb964cbdb3f69ef291af1bf5803341901b43bf7
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:52:08 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:52:01.040Z
-Commit: Add multi-language support
+Timestamp: 2026-04-22T03:52:08.332Z
+Commit: Resolve timezone issues in date handling
