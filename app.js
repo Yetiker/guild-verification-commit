51ed2ae8c70eb5847f9046dd8f9195d91b2e49ed
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:51:32 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:51:29.859Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-22T03:51:31.897Z
+Commit: Fix memory leak in event handlers
