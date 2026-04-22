b1b692a46b4d988fcf706003487a3f92282086d3
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:52:23 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:52:21.635Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-22T03:52:22.694Z
+Commit: Fix CSS styling bug on mobile devices
