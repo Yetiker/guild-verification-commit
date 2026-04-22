915f32f992d4aef3edc255718f4238292a7d3f9e
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:52:01 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:51:55.998Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-22T03:52:01.040Z
+Commit: Add multi-language support
