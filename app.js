a316dcde5c937446b2a2f0e22b1dc9657469828e
Yetiker <yetikerhakan@gmail.com>
2026-04-22 06:52:27 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T03:52:24.459Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-22T03:52:27.208Z
+Commit: Fix broken links in documentation
